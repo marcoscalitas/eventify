239a68d739369549eee216d2e0ce25851822bc10
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-16 10:16:59 +0100

    refactor: remove created_by and updated_by from Favorite

--- reservations/models/favorite.py
+++ reservations/models/favorite.py
@@ -12,18 +12,6 @@ class Favorite(models.Model):
     deleted_at = models.DateTimeField(null=True, blank=True, default=None, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.SET_NULL,
-        null=True, blank=True,
-        related_name="favorite_created",
-    )
-    updated_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.SET_NULL,
-        null=True, blank=True,
-        related_name="favorite_updated",
-    )
 
     objects = SoftDeleteManager()
     all_objects = AllObjectsManager()
