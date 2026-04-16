b7c41f04165d77195ec163f68ce8d7317c11ef93
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-16 09:58:27 +0100

    refactor: remove created_by and updated_by from Category

--- reservations/models/category.py
+++ reservations/models/category.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
@@ -13,18 +12,6 @@ class Category(models.Model):
     deleted_at = models.DateTimeField(null=True, blank=True, default=None, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.SET_NULL,
-        null=True, blank=True,
-        related_name="category_created",
-    )
-    updated_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.SET_NULL,
-        null=True, blank=True,
-        related_name="category_updated",
-    )
 
     objects = SoftDeleteManager()
     all_objects = AllObjectsManager()
