fc11f536a463826cc6a0d9bd9e1098742c89b912
Marcos Calitas <marcoscalitas@gmail.com>
2026-04-16 10:13:42 +0100

    refactor: remove created_by and updated_by from Review

--- reservations/models/review.py
+++ reservations/models/review.py
@@ -17,18 +17,6 @@ class Review(models.Model):
     deleted_at = models.DateTimeField(null=True, blank=True, default=None, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.SET_NULL,
-        null=True, blank=True,
-        related_name="review_created",
-    )
-    updated_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.SET_NULL,
-        null=True, blank=True,
-        related_name="review_updated",
-    )
 
     objects = SoftDeleteManager()
     all_objects = AllObjectsManager()
